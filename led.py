d4cf4131fb9291a5550ceeceee9ad3effc5aef48
ezjinha <116865552+ezjinha@users.noreply.github.com>
2022-11-03 14:32:33 +0900

    Create led.py

---
+++ led.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)          # GPIO BCM 모드 설정
+
+LED = 21 # BCM P21
+
+GPIO.setup(LED, GPIO.OUT)
+
+while 1:                        # 무한 반복 - LED On/Off
+    GPIO.output(LED, GPIO.HIGH)
+    time.sleep(0.5)
+
+    GPIO.output(LED, GPIO.LOW)
+    time.sleep(0.5)
